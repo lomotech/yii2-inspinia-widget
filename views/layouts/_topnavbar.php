24bb701f652c2ea4c06e0767194b9342ad4f5316
xuchao <85825770@qq.com>
2017-04-19 11:57:26 +0800

    页头

--- views/layouts/_topnavbar.php
+++ views/layouts/_topnavbar.php
@@ -1,4 +1,6 @@
 <?php
+use yii\helpers\Url;
+use yii\helpers\Html;
 ?>
 <div class="row border-bottom">
     <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
@@ -7,9 +9,13 @@
         </div>
         <ul class="nav navbar-top-links navbar-right">
             <li>
-                <a href="#">
-                    <i class="fa fa-sign-out"></i> 退出
-                </a>
+                <?= Html::a('<i class="fa fa-sign-out"></i>' . Yii::t('admin', 'Logout'), Url::to(['/admin/security/logout']), [
+                    'title' => Yii::t('admin', 'Sign Out'),
+                    'data' => [
+                        'method' => 'post',
+                        'confirm' => Yii::t('admin', 'You can improve your security further after logging out by closing this opened browser')
+                    ]
+                ]); ?>
             </li>
         </ul>
     </nav>
