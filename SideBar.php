7a1ab9b82b61e17f67040d43fc500cae2ef8b22b
xuchao <85825770@qq.com>
2017-04-19 11:52:16 +0800

    边栏

--- SideBar.php
+++ SideBar.php
@@ -9,14 +9,15 @@ namespace xutl\inspinia;
 
 use Yii;
 use yii\helpers\Url;
+use yii\widgets\Menu;
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 
 /**
- * Class SideBar
+ * Inspinia SideBar Widget
  * @package xutl\inspinia
  */
-class SideBar extends \yii\widgets\Menu
+class SideBar extends Menu
 {
     /**
      * @var string the template used to render the body of a menu which is a link.
@@ -24,7 +25,7 @@ class SideBar extends \yii\widgets\Menu
      * while `{label}` will be replaced with the link text.
      * This property will be overridden by the `template` option set in individual menu items via [[items]].
      */
-    public $linkTemplate = '<a href="{url}">{icon} {label}</a>';
+    public $linkTemplate = '<a href="{url}">{icon} {label} {arrow}</a>';
 
     /**
      * @var string the template used to render the body of a menu which is NOT a link.
@@ -40,4 +41,73 @@ class SideBar extends \yii\widgets\Menu
     public $submenuTemplate = "\n<ul class=\"nav nav-second-level collapse\">\n{items}\n</ul>\n";
 
     public $activateParents = true;
+
+    /**
+     * Recursively renders the menu items (without the container tag).
+     * @param array $items the menu items to be rendered recursively
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+            $class = [];
+            if ($item['active']) {
+                $class[] = $this->activeCssClass;
+            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+
+            $menu = $this->renderItem($item);
+            if (!empty($item['items'])) {
+                $submenuTemplate = ArrayHelper::getValue($item, 'submenuTemplate', $this->submenuTemplate);
+                $menu .= strtr($submenuTemplate, [
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+
+        return implode("\n", $lines);
+    }
+
+    /**
+     * Renders the content of a menu item.
+     * Note that the container and the sub-menus are not rendered here.
+     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
+     * @return string the rendering result
+     */
+    protected function renderItem($item)
+    {
+        if (isset($item['url'])) {
+            $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
+
+            return strtr($template, [
+                '{icon}' => Html::tag('i', '', ['class' => 'fa fa-lg fa-fw ' . $item['icon']]),
+                '{url}' => Html::encode(Url::to($item['url'])),
+                '{label}' => $item['label'],
+                '{arrow}' => isset($item['items']) ? Html::tag('span','',['class' => 'fa arrow']) : '',
+            ]);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
+
+            return strtr($template, [
+                '{label}' => $item['label'],
+            ]);
+        }
+    }
 }
