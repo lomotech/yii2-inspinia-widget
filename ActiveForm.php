166daeb6d45945df3d9c0d45c4c5a421bda65831
xuchao <85825770@qq.com>
2017-04-21 13:37:51 +0800

    update

---
+++ ActiveForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\inspinia;
+
+class ActiveForm extends \yii\bootstrap\ActiveForm
+{
+    /**
+     * @var string the default field class name when calling [[field()]] to create a new field.
+     * @see fieldConfig
+     */
+    public $fieldClass = 'xutl\inspinia\ActiveField';
+}
